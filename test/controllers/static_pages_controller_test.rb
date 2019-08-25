be12529cfdce84c6d78c118dace85db2622135f0
pndemo <ndemopaul1@gmail.com>
2019-08-25 11:17:31 +0300

    User auth (#11)
    * Update sign up route
    
    * Update sign up page
    
    * Add sign up integration tests
    
    * Refactor static pages integration tests
    
    * Add users controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,31 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @basic_title = 'Micro news'
-  end
+  include ApplicationHelper
 
   test 'should get root' do
     get root_path
     assert_response :success
-    assert_select 'title', @basic_title
+    assert_select 'title', full_title
   end
 
   test 'should get help' do
     get help_path
     assert_response :success
-    assert_select 'title', "Help | #{@basic_title}"
+    assert_select 'title', full_title('Help')
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
-    assert_select 'title', "About | #{@basic_title}"
+    assert_select 'title', full_title('About')
   end
 
   test 'should get contact' do
     get contact_path
     assert_response :success
-    assert_select 'title', "Contact | #{@basic_title}"
+    assert_select 'title', full_title('Contact')
   end
 end
